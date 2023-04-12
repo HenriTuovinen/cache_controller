8ff51f5ee681ad08534c6f907382e57889000f43
Henri Tuovinen <henrituovinen7@gmail.com>
2023-04-12 20:55:41 +0300

    rewrote tests accordingly and fixed some priors

--- src/test/scala/cc/CacheControllerSpec.scala
+++ src/test/scala/cc/CacheControllerSpec.scala
@@ -33,7 +33,11 @@ class CacheControllerSpec extends AnyFlatSpec with ChiselScalatestTester {
 
 
 
-    test(new Cache(2,3,4)) { dut =>
+    test(new Cache(4,1,3)) { dut =>
+
+
+      
+
       //val buffSource = io.Source.fromFile("/resources/")
       //dut.io.addr.peek()
       //dut.io.datain.peek()
@@ -41,30 +45,37 @@ class CacheControllerSpec extends AnyFlatSpec with ChiselScalatestTester {
       //dut.io.we.peek()
 
       //println("before input" + dut.io.dataout.peek().litValue)
-      //printf("paskaaa")
+      //printf("test")
 
-      dut.io.we.poke(1.U)
-      dut.io.addr.poke(7.U)
+      dut.clock.step(10)
+
+      dut.io.addr.poke(3.U)
+      dut.io.tag.poke(1.U) 
       dut.io.datain.poke(2.U)
+      dut.io.we.poke(true.B)
+
 
       dut.clock.step(10)
 
       //println("after input" + dut.io.dataout.peek().litValue)
-      dut.io.dataout.expect("hb".U)
+
+      dut.io.dataout.expect(2.U)
+      dut.io.tagout.expect(1.U)
+      dut.io.valid.expect(true.B)
     }
   }
 
-/*
+
   it should "just peekking into controller" in {                            //test the cache alone
     test(new CacheController(2,3,2)) { dut =>
       //val buffSource = io.Source.fromFile("/resources/")
       
     }
   }
-*/
 
 
-/*
+
+
   it should "testing basic operation with peek poke" in {
     test(new CacheController(2,3,2)) { dut =>
       //val buffSource = io.Source.fromFile("/resources/")
@@ -79,12 +90,12 @@ class CacheControllerSpec extends AnyFlatSpec with ChiselScalatestTester {
       dut.memout.data.peek()
 
 
-      dut.cpuin.addr.poke("010".U)
+      dut.cpuin.addr.poke("b010".U)
       dut.cpuin.valid.poke(true.B)
-      dut.cpuin.rw.poke(true.B)
-      dut.cpuin.data.poke("11".U)
+      dut.cpuin.rw.poke(false.B)
+      dut.cpuin.data.poke("b11".U)
 
-      dut.memin.data.poke("01".U)
+      dut.memin.data.poke("b01".U)
       dut.memin.valid.poke(true.B)
       dut.memin.ready.poke(true.B)
 
@@ -99,8 +110,18 @@ class CacheControllerSpec extends AnyFlatSpec with ChiselScalatestTester {
       dut.memout.req.peek()
       dut.memout.rw.peek()
       dut.memout.data.peek()
+
+
+
+      dut.cpuout.data.expect("b01".U)
+      dut.cpuout.valid.expect(true.B)
+      dut.cpuout.busy.expect(true.B)
+      dut.cpuout.hit.expect(true.B)
+
+      dut.memout.addr.expect("b010".U)
+      dut.memout.req.expect(false.B)
+      dut.memout.rw.expect(false.B)
+      dut.memout.data.expect("b11".U)
     }
   }
-  */
-
 }
